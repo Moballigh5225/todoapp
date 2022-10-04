1ed26ee4b7f9462c4b7ba03cd7961a0a086a670c
Moballighul islam <moballigh5225@gmail.com>
2022-10-04 19:07:44 +0530

    todo added

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+import Input from "./components/Input/Input";
+import TodoItem from "./components/TodoItem/TodoItem";
+
+import { useSelector } from "react-redux";
+import { selectTodoList } from "./features/todoSlice";
+
+function App() {
+  const todoList = useSelector(selectTodoList);
+  return (
+    <div className="app">
+      <div className="app__container">
+        <div className="title">Todo App</div>
+        <div className="app__todoContainer">
+          {todoList.map((item) => (
+            <TodoItem name={item.item} done={item.done} id={item.id} />
+          ))}
+        </div>
+        <Input />
+      </div>
+    </div>
+  );
+}
+
+export default App;
